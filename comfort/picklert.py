5c776c6874895c8f7ae6e1ffd6d304b8f87764fb
nickpowersys <nbprofessional@gmail.com>
2016-07-20 16:50:59 -0500

    Added instructions on pickling raw data files from command line.

--- comfort/picklert.py
+++ comfort/picklert.py
@@ -5,6 +5,55 @@ from comfort.configparser_read import THERMOSTATS_FILE, POSTAL_FILE,      \
     DATA_DIR
 
 
+"""This script file creates a Python pickle file from a raw data file that
+contains either operating data from thermostats or temperature data (indoor
+or outdoor). The pickle file can be loaded from the same project later and
+read as a Python dict (a hash file in which the indexes or keys are
+combinations of ID numbers and time stamps).
+
+Run the script from the command line. It may be necessary to add the full
+path for the directory 'comfort' to the PYTHONPATH first. For example:
+
+PYTHONPATH = '/home/tl_cycling/comfort'
+import sys
+sys.path.append(PYTHONPATH)
+
+The  columns in the input file should form an intersecting set with the
+headings listed in config.ini under the [file_headers] section. The
+leading column should have a thermostat or location ID in numeric form.
+
+For example, a file containing a column that has a thermostat ID could have
+the default heading for the first column as shown in the example
+data/cycles.csv file, 'ThermostatId', or the heading could be just 'Id'.
+
+The file contents and config.ini just need to match.
+
+See the example input files in the data directory to see the general format.
+
+The script takes a single input file as an argument, such as 'cycles.csv',
+'inside.csv', or 'outside.csv'. This is the only required argument.
+
+The first optional argument is a state abbreviation, as will be explained.
+
+The other arguments are taken as defaults ('thermostats.csv',
+'us_postal_codes.csv') unless specified with an option.
+
+To run from the command line, the general form is:
+
+    python picklert.py [Input file] --states=[Two-letter abbreviation as string]
+
+An example is:
+
+    python picklert.py 'cycles.csv' --states='TX'
+
+This example is based on the assumption that only a single state is of interest.
+
+Data from all states in the input file can be included by leaving out the --states option.
+
+Otherwise, multiples states can be selected, such as --states='TX,IA'.
+"""
+
+
 @click.command()
 @click.argument('rawfile')
 @click.option('--states', default=None,
