2f80447cb2f7f00b5d1f81567e232094578a4af4
nickpowersys <nbprofessional@gmail.com>
2016-07-23 22:34:18 -0500

    Revised output_path

--- docs/conf.py
+++ docs/conf.py
@@ -220,7 +220,7 @@ def run_apidoc(_):
     modules = ['caar']
     for module in modules:
         cur_dir = os.path.abspath(os.path.dirname(__file__))
-        output_path = os.path.join(cur_dir, module, 'docs')
+        output_path = cur_dir
         cmd_path = 'sphinx-apidoc'
         if hasattr(sys, 'real_prefix'):  # Check to see if we are in a virtualenv
             # If we are, assemble the path manually
