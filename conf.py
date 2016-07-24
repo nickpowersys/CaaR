3fa8cafbd5842b6797dca35776165fe8f661e51b
nickpowersys <nbprofessional@gmail.com>
2016-07-24 11:38:24 -0500

    Added rst extension to caar in index.rst toc

--- conf.py
+++ conf.py
@@ -389,3 +389,5 @@ epub_exclude_files = ['search.html']
 # If false, no index is generated.
 #epub_use_index = True
 
+# Autodoc
+autodoc_member_order = 'bysource'
--- index.rst
+++ index.rst
@@ -11,7 +11,7 @@ Contents:
 .. toctree::
    :maxdepth: 4
 
-   caar
+   caar.rst
 
 
 Indices and tables
