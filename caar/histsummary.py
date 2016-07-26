22457c77ff982e9c57e75a6823102a01cca14ec9
nickpowersys <nbprofessional@gmail.com>
2016-07-26 01:33:27 -0500

    Added documentation and df_select_ids function

--- caar/histsummary.py
+++ caar/histsummary.py
@@ -1,105 +1,217 @@
 import datetime as dt
-import math
 
 import numpy as np
 import pandas as pd
 
-from caar.histdaily import location_id_of_thermo
 
 from caar.configparser_read import OUTSIDE_TIMESTAMP_LABEL,               \
-    CYCLE_START_TIME, CYCLE_END_TIME, THERMO_ID_FIELD, INSIDE_TEMP_FIELD
+    CYCLE_START_TIME, CYCLE_END_TIME, THERMO_ID_FIELD, INSIDE_TEMP_FIELD, \
+    THERMOSTAT_DEVICE_ID, THERMOSTAT_LOCATION_ID
 
 
 def days_of_data_by_id(df):
-    """Return the number of calendar days that have data for each ID."""
+    """Return pandas DataFrame with ID as index and the number of calendar
+    days of data as values.
+
+    Args:
+        df (pandas DataFrame): DataFrame as created by **history** module.
+
+    Returns:
+        days_data_df (pandas DataFrame): DataFrame with count
+        ('Days') for each ID.
+    """
     days_data_df = (df.groupby([df.index.get_level_values(level=0),
                                 pd.TimeGrouper('D', level=1)])
                     .count()
                     .groupby(level=0)
                     .count())
+    days_data_df.columns = ['Days']
+    return days_data_df
 
 
-def consecutive_days_of_observations(id, cycles_df, inside_df, outside_df):
+def consecutive_days_of_observations(id, cycles_df, inside_df, outside_df,
+                                     thermostats_file):
+    """
+    Returns a pandas DataFrame with a row for each date range indicating the
+    number of consecutive days of data across all DataFrames given as
+    arguments. The starting and ending day of each date range are also given.
 
+    Args:
+        id (int): The ID of the thermostat.
+
+        cycles_df (pandas DataFrame): DataFrame as created by **history**
+        module.
+
+        inside_df (pandas DataFrame): DataFrame as created by **history**
+        module.
+
+        outside_df (pandas DataFrame): DataFrame as created by **history**
+        module.
+
+        thermostats_file(str): Path of thermostats file.
+
+    Returns:
+        consecutive_days_df (pandas DataFrame): DataFrame with 'First Day',
+        'Last Day', and count ('Consecutive Days') for each set of consecutive
+        days, for the specified ID.
+    """
     obs_counts = daily_cycle_and_temp_obs_counts(id, cycles_df, inside_df,
-                                                 outside_df)
+                                                 outside_df, thermostats_file)
     streaks = []
     streak_days = 0
     one_day = pd.Timedelta(days=1)
-    # last_day_of_obs = obs_counts.index.max()
     day0 = obs_counts.index[1]  # Second day because first may be partial
     day1 = day0 + one_day
-    while day1 <= obs_counts.index[-2]:  # Next to last because last may be partial
+    while day1 <= obs_counts.index[-2]:  # Next to last (last may be partial)
         if day1 in obs_counts.index:
             streak_days += 1
             day1 += one_day
         else:
-            if streak_days >= 3:  # Ignore first and last day of streak because may be partial
+            if streak_days >= 3:  # Ignore first and last day (may be partial)
                 first_day = day0 + one_day
                 last_day = day1 - one_day
                 total_days = (last_day - first_day + one_day) / one_day
-                first_day_dt = dt.datetime(first_day.year, first_day.month,
-                                           first_day.day)
-                last_day_dt = dt.datetime(last_day.year, last_day.month,
-                                           last_day.day)
+                first_day_dt = dt.date(first_day.year, first_day.month,
+                                       first_day.day)
+                last_day_dt = dt.date(last_day.year, last_day.month,
+                                      last_day.day)
                 streaks.append((id, first_day_dt, last_day_dt, total_days))
             streak_days = 0
             day0 = day1 + one_day
             day1 = day0 + one_day
-    #streaks_arr = np.array(streaks, dtype=[('Thermostat Id', np.uint32),
-    #                                       ('First day', 'datetime64[D]'),
-    #                                       ('Last day', 'datetime64[D]'),
-    #                                       ('Total days', np.uint32)])
     streaks_arr = np.array(streaks)
-    #np.array(streaks_arr[::-1].sort(order='Total days'))
-    streaks_arr[streaks_arr[:, 3].argsort()]
-    return streaks_arr
+    streaks_arr[streaks_arr[:, 1].argsort()]
+    streaks_df = pd.DataFrame(data=streaks_arr,
+                              columns=['ID', 'First day', 'Last day',
+                                       'Consecutive days'])
+    return streaks_df
 
 
-def daily_cycle_and_temp_obs_counts(id, cycles_df, inside_df, outside_df):
-    """Returns a NumPy array with id, first day, first day day of week,
-    last day, last day day of week, total week days , total weekend days, total days.
+def daily_cycle_and_temp_obs_counts(id, cycles_df, inside_df, outside_df,
+                                    thermostats_file):
+    """Returns a pandas DataFrame with the count of observations of each type
+    of data given in the arguments (cycles, temperatures, outside
+    temperatures, by day. Only days in which all data types have one or more
+    observations are included.
+
+    Args:
+        id (int): The ID of the thermostat.
+
+        cycles_df (pandas DataFrame): DataFrame as created by **history**
+        module.
+
+        inside_df (pandas DataFrame): DataFrame as created by **history**
+        module.
+
+        outside_df (pandas DataFrame): DataFrame as created by
+        **history** module.
+
+        thermostats_file(str): Path of thermostats file.
+
+    Returns:
+        daily_obs_df (pandas DataFrame): DataFrame with index of the date, and
+        values of 'Cycles_obs', 'Inside_obs', and 'Outside_obs'.
     """
     idx = pd.IndexSlice
-    #cycles_and_inside = (df.loc[idx[id, :], :]
-    #                     for df in [cycles_df, inside_df])
     cycles = cycles_df.loc[idx[id, :], :]
     inside = inside_df.loc[idx[id, :], :]
-    outside_records = outside_df.loc[idx[location_id_of_thermo(id), :], :]
+    outside_data = True if isinstance(outside_df, pd.DataFrame) else False
+    if outside_data:
+        location_id = location_id_of_thermo(id, thermostats_file)
+        outside_records = outside_df.loc[idx[location_id, :], :]
+        df_list = [cycles, inside, outside_records]
+    else:
+        df_list = [cycles, inside]
     # Get df's with number of observation by day, for each of 3 types of data
-    dfs = [daily_data_points_by_id(df) for df in [cycles, inside,
-                                                  outside_records]]
-    cycles, inside, outside = (df.set_index(df.index.droplevel()) for df in dfs)
+    dfs = [daily_data_points_by_id(df) for df in df_list]
+    if outside_data:
+        cycles, inside, outside = (df.set_index(df.index.droplevel())
+                                   for df in dfs)
+    else:
+        cycles, inside = (df.set_index(df.index.droplevel()) for df in dfs)
     cycles_inside = pd.merge(cycles, inside, left_index=CYCLE_START_TIME,
                              right_index=True, how='inner')
-    return (pd.merge(cycles_inside, outside, left_index=True,
-                     right_index=OUTSIDE_TIMESTAMP_LABEL)
-            .rename(columns={CYCLE_END_TIME: 'Cycles_obs', 'Degrees_x': 'Inside_obs',
-                             'Degrees_y': 'Outside_obs'}))
+    if outside_data:
+        return (pd.merge(cycles_inside, outside, left_index=True,
+                         right_index=OUTSIDE_TIMESTAMP_LABEL)
+                .rename(columns={CYCLE_END_TIME: 'Cycles_obs',
+                                 'Degrees_x': 'Inside_obs',
+                                 'Degrees_y': 'Outside_obs'}))
+    else:
+        return cycles_inside.rename(columns={CYCLE_END_TIME: 'Cycles_obs',
+                                             'Degrees': 'Inside_obs'})
+
+
+def location_id_of_thermo(thermo_id, thermostats_file):
+    thermostat_df = pd.read_csv(thermostats_file,
+                                usecols=[THERMOSTAT_DEVICE_ID,
+                                         THERMOSTAT_LOCATION_ID],
+                                index_col=0)
+    idx = pd.IndexSlice
+    return thermostat_df.loc[idx[thermo_id, THERMOSTAT_LOCATION_ID]]
 
 
 def daily_data_points_by_id(df, id=None):
-    """Return a DataFrame with MultiIndex of primary ID and time grouping,
+    """Return a pandas DataFrame with MultiIndex of ID and time grouping,
     and the count of non-null raw data points per id and time group as values.
+
+        Args:
+        df (pandas DataFrame): DataFrame as created by **history** module.
+
+        id (Optional: int): The ID of a thermostat.
+
+    Returns:
+        daily_obs_df (pandas DataFrame): DataFrame with index of the date, and
+        values of 'Cycles_obs', 'Inside_obs', and 'Outside_obs'.
     """
-    # 1) Groups the dataframe by the primary ID and by time.
+    # 1) Groups the DataFrame by the primary ID and by time.
     # 2) Gives count of records within groups.
     if id is not None:
         idx = pd.IndexSlice
         df = df.loc[idx[id, :], :]
-    return (df.groupby([df.index.get_level_values(level=0),
-                       pd.TimeGrouper('D', level=1)])
-            .count())
-
-
-
+    daily_df = (df.groupby([df.index.get_level_values(level=0),
+                            pd.TimeGrouper('D', level=1)])
+                .count())
+    return daily_df
 
 
 def counts_by_primary_id(df):
+    """Return dict with IDs as keys and total number (int) of observations
+     of data as values, based on the DataFrame (df) passed as an argument."""
     return (df
             .groupby(level=0)
             .count()
             .to_dict())
+
+
+def df_select_ids(df, id_or_ids):
+    """Return pandas DataFrame that is restricted to a particular ID or IDs
+    (thermostat ID, or location ID in the case of outside temperatures).
+
+    Args:
+        df (pandas DataFrame): DataFrame that has been created by a function
+        in the **history** or **histsummary** modules (it must have a numeric
+        id as the first or only index column).
+
+        id_or_ids (int, list of ints, or tuple): A tuple should hav the form
+        (min_ID,max_ID)
+
+    Returns:
+        daily_obs (pandas DataFrame)
+
+    """
+    idx = pd.IndexSlice
+    if isinstance(id_or_ids, tuple):
+        min_id, max_id = id_or_ids[0], id_or_ids[1]
+        if len(df.columns) > 1:
+            return pd.DataFrame(df.loc[idx[min_id:max_id+1, :], :])
+        else:
+            return pd.DataFrame(df.loc[idx[min_id:max_id+1], :])
+    else:
+        if len(df.columns) > 1:
+            return pd.DataFrame(df.loc[idx[id_or_ids, :], :])
+        else:
+            return pd.DataFrame(df.loc[idx[id_or_ids], :])
 
 
 def squared_avg_daily_data_points_per_id(df):
@@ -130,74 +242,10 @@ def matching_ids_all_dfs(cycles_df, inside_df, outside_df):
     indoor and outdoor temperature data.
     """
     dfs = [cycles_df, inside_df, outside_df]
-    cycles_ids, inside_ids, outside_ids = (df.get_level_values(level=0) for df in dfs)
+    cycles_ids, inside_ids, outside_ids = (df.get_level_values(level=0)
+                                           for df in dfs)
     cycles_intersect_inside = cycles_ids.intersection(inside_ids)
     return dict(map(location_id_of_thermo(cycles_intersect_inside)))
-
-
-def distance_by_id_across_all_data_points(cycles_df, inside_df, outside_df):
-    ids = []
-    dfs = [cycles_df, inside_df, outside_df]
-    # do i want to have days of data instead of raw counts?
-    # then the average counts factors in how much data there is for cycles, inside and outside
-    # counts = list(map(counts_by_primary_id, dfs))
-    days_of_data = list(map(days_of_data_by_id, dfs))
-    squared_avg_daily_data_pts_by_id = list(map(squared_avg_daily_data_points_per_id, dfs))
-    # cts_sqrd = list(map(counts_by_primary_id_squared, dfs))
-    # need to know the ids for these dfs
-    matching_ids = matching_ids_all_dfs(cycles_df, inside_df, outside_df)
-    for id in matching_ids:
-        sum = 0
-        days = 0
-        # cts_of_id = (df[id] for df in counts)
-        # ignore days_of_data as above.
-        # for the id, get the data by day for each df
-        # concat join inner on the index level
-        # get the number of records of the joined dfs
-        days_of_id = (df[id] for df in days_of_data)
-        min_days = min(days_of_id)
-        # do i want to match up all the days for the filtered data
-        # and just take those days?
-        # would need to match the dataframes by the indexes in level=1
-        # cts_sqrd_of_id = (df[id] for df in cts_sqrd)
-        avg_sqrd_of_id = (df[id] for df in squared_avg_daily_data_pts_by_id)
-        # for i, id_cts in enumerate(cts_sqrd_of_id):
-        for i, id_cts in enumerate(avg_sqrd_of_id):
-            sum += id_cts[i]
-        dist = math.sqrt(sum)
-        ids.append((id, min_days, dist))
-    return ids
-
-
-def number_days_for_cycle_and_temp_data_by_id(cycles_df, inside_df, outside_df):
-    # cycle_df_daily = daily_data_points_by_id(df)
-    dfs = [cycles_df, inside_df, outside_df]
-    daily_data_counts = list(map(daily_data_points_by_id, dfs))
-    pd.concat(daily_data_counts, axis=1, join='inner')
-    # dfs = [cycles_df, inside_df, outside_df]
-    # # list of tuples with id as the leading id instead?
-    # # the ids are probably unsorted
-    # # get the counts and
-    #
-    # counts = [df.groupby(level=0)
-    #             .count() for df in dfs]
-    # counts_squared = [df.apply(lambda x: x**2)
-    #                                           for df in counts]
-    # matching_ids = matching_ids_all_dfs(cycles_df, inside_df, outside_df)
-    # # create a data structure to hold the ids and the squared counts
-    # idx = pd.IndexSlice
-    # squared_count_by_id = []
-    # for id in matching_ids:
-    #     sum = 0
-    #     # counts_by_id = []
-    #     for df in counts:
-    #         counts_by_id = (df[idx[id,:],0], counts[])
-    #     for i, df in enumerate(counts_squared):
-    #
-    #         sum += df[idx[id,:],0]
-    #     distance = math.sqrt(sum)
-    #     sum = math.sqrt(cycles_sqrd[idx[id,:],0] + inside_sqrd[idx[id,:]] + outside_sqrd[idx[id,:]])
-    #     squared_count_by_id.append((id, sum, ))
 
 
 def first_full_day_of_inside_temperature_data(df):
@@ -232,19 +280,21 @@ def number_of_days(df):
 
 
 def start_of_first_full_day_df(cycle_df):
-    """"Returns datetime.datetime value of the very beginning of the firt
+    """"Returns datetime.datetime value of the very beginning of the first
     full day for which data is given in a pandas DataFrame. The DataFrame
     must have a MultiIndex in which the second level of the index (level=1)
     contains timestamps."""
-    earliest_timestamp = cycle_df.index.get_level_values(level=1).min()  # min() is minimum (earliest) timestamp in df
+    earliest_timestamp = cycle_df.index.get_level_values(level=1).min()
     earliest_full_day = earliest_timestamp + pd.Timedelta(days=1)
-    start_earliest_full_day = dt.datetime(earliest_full_day.year, earliest_full_day.month,
-                                          earliest_full_day.day, hour=0, minute=0)
+    start_earliest_full_day = dt.datetime(earliest_full_day.year,
+                                          earliest_full_day.month,
+                                          earliest_full_day.day, hour=0,
+                                          minute=0)
     return start_earliest_full_day
 
 
 def start_of_last_full_day_df(cycle_df):
-    last_timestamp = cycle_df.index.get_level_values(level=1).max() # max() is maximum (latest) timestamp in df
+    last_timestamp = cycle_df.index.get_level_values(level=1).max()
     last_full_day = last_timestamp - pd.Timedelta(days=1)
     start_last_full_day = dt.datetime(last_full_day.year, last_full_day.month,
                                       last_full_day.day, hour=0, minute=0)
