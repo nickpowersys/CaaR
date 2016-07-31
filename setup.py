7df51f4aeab23f1d90f4047977d5611a0c06206a
nickpowersys <nbprofessional@gmail.com>
2016-07-31 13:05:48 -0500

    Version at 0.0.6

--- setup.py
+++ setup.py
@@ -3,14 +3,18 @@ from setuptools import find_packages, setup
 
 setup(
     name='caar',
-    version='0.0.5',
+    version='0.0.6',
     url='http://github.com/nickpowersys/CaaR/',
     license='BSD 3-Clause License',
     author='Nicholas A. Brown',
-    tests_require=['pytest'],
     author_email='nbprofessional@gmail.com',
     description='Accelerating analysis of data on temperatures and '
                 'thermostat-driven loads.',
+    # install_requires=['numpy>=1.11.1',
+    #  'pandas>=0.18.1',
+    #  'scipy>=0.17.1',
+    #  'click>=6.6',
+    #  ],
     packages=find_packages(),
     package_data={
       'caar': ['./config.ini'],
@@ -20,6 +24,7 @@ setup(
     platforms='any',
     classifiers=[
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Development Status :: 4 - Beta',
         'Natural Language :: English',
         'License :: OSI Approved :: BSD 3-Clause License',
