84dee56df90d9181d1e79c3246ef389462f0ca17
nickpowersys <nbprofessional@gmail.com>
2016-09-17 21:39:26 -0500

    Put imports as they are in init

--- configure_console_session.py
+++ configure_console_session.py
@@ -5,10 +5,31 @@ PYTHONPATH = '/home/nick/PycharmProjs/tl_cycling/backports'
 sys.path.append(PYTHONPATH)
 PYTHONPATH = '/home/nick/PycharmProjs/tl_cycling/backports/configparser'
 sys.path.append(PYTHONPATH)
-from comfort import cleanthermostat as ct
-from comfort import history as hi
-from comfort import histdaily as hd
-from comfort import histsummary as hs
+from caar.cleanthermostat import dict_from_file
+from caar.cleanthermostat import detect_columns
+from caar.cleanthermostat import pickle_from_file
+
+from caar.history import create_cycles_df
+from caar.history import create_inside_df
+from caar.history import create_outside_df
+from caar.history import random_record
+
+from caar.histsummary import days_of_data_by_id
+from caar.histsummary import consecutive_days_of_observations
+from caar.histsummary import daily_cycle_and_temp_obs_counts
+from caar.histsummary import daily_data_points_by_id
+from caar.histsummary import df_select_ids
+from caar.histsummary import df_select_datetime_range
+from caar.histsummary import count_of_data_points_for_each_id
+from caar.histsummary import count_of_data_points_for_select_id
+from caar.histsummary import location_id_of_thermo
+
+from caar.timeseries import time_series_cycling_and_temps
+from caar.timeseries import on_off_status
+from caar.timeseries import temps_arr_by_freq
+from caar.timeseries import plot_cycles_xy
+from caar.timeseries import plot_temps_xy
+
 
 
 
