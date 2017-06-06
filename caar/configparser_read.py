601b833f9b8afe161b9ba63dd1d679fcf188c40d
nickpowersys <nbprofessional@gmail.com>
2017-06-05 23:40:31 -0500

    Removed backports namespace

--- caar/configparser_read.py
+++ caar/configparser_read.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import, division, print_function
-from backports.configparser import ConfigParser
+from configparser import ConfigParser
 import os.path
 import sys
 
